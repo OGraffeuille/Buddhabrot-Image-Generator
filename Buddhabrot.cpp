4454e2c3635d0975cefbf84dfab88d2f33052f85
Olivier <34634950+OGraffeuille@users.noreply.github.com>
2022-09-19 16:42:18 +1200

    Update Buddhabrot.cpp
    Added customisability in exporting PNG, cleaned up code

--- Buddhabrot.cpp
+++ Buddhabrot.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_DEPRECATE // to avoid https://stackoverflow.com/questions/14386/fopen-deprecated-warning
 #include <iostream>
 #include <tuple>
 #include <algorithm> 
@@ -22,21 +23,25 @@ time_point<Clock> start;
 
 // Returns a RGB tuple based on an alpha value between [0 1], and palette chosen
 std::tuple<int, int, int> find_rgb(double a, const std::string& palette) {
+
 	a = std::min(std::max(a, 0.), 1.);
 	int r, g, b;
 	if (palette == "Sunset") {
 		r = std::max(std::min((int)(255 - 510 * a), 255), 0);
 		g = std::max(std::min((int)(510 * a - 255), 255), 0);
 		b = (int)(255 * -a * a * (2 * a - 3));
-	} else if (palette == "Gold") {
-		r = (int)(128 + 127 * std::cos((a) * 4));
-		g = (int)(128 + 127 * std::cos((a + 0.333) * 4));
-		b = (int)(128 + 127 * std::cos((a + 0.666) * 4));
-	} else if (palette == "Fire") {
+	}
+	else if (palette == "Gold") {
+		r = (int)(128 + 127 * std::cos((a - 0.166) * 4.6));
+		g = (int)(128 + 127 * std::cos((a + 0.166) * 4.6));
+		b = (int)(128 + 127 * std::cos((a + 0.500) * 4.6));
+	}
+	else if (palette == "Fire") {
 		r = std::min((int)(a * (3 - 2 * a) * 255), 255);
 		g = (int)(a * a * 250);//std::max(0, (int)(510 * a - 255));
-		b = 0;	
-	} else { // Greyscale
+		b = 0;
+	}
+	else { // Greyscale
 		r = (int)(255 * a);
 		g = (int)(255 * a);
 		b = (int)(255 * a);
@@ -60,7 +65,7 @@ public:
 	int max;
 
 	// Number of successful points found and recorded
-	int pointsDoneTot;
+	double pointsDoneTot;
 
 	// Size of matrix to store counts
 	int xDet;
@@ -87,7 +92,7 @@ public:
 		xMax = xMaxLim;
 		yMin = yMinLim;
 		yMax = yMaxLim;
-		count.resize(xDet*yDet, 0);
+		count.resize(xDet * yDet, 0);
 		dist.resize(max + 2, 0);
 	}
 
@@ -107,79 +112,18 @@ public:
 		std::cout << "Starting loading matrix...\n";
 		tic();
 
+		std::vector<char> buffer(1, 0); //reads only the first 1024 bytes
 		inFile >> pointsDoneTot >> min >> max >> xDet >> yDet >> xMin >> xMax >> yMin >> yMax;
+		count.reserve(xDet * yDet);
 		int val;
 		while (!inFile.eof()) {
+			inFile.read(buffer.data(), buffer.size());
+			//std::streamsize s = inFile.gcount();
 			inFile >> val;
 			count.push_back(val);
 		}
-		dist.resize(max + 2, 0);
 
 		std::cout << "Finished loading matrix (" << toc() << "s).\n\n";
-	}
-
-	// Creates a buddhabrot in the matrix, using calculations with complex numbers (SLOW)
-	void CalcComplex(int pointsToDo) {
-		std::cout << "Starting calculating matrix...\n";
-		tic();
-
-		// Seed, random number generator, distribution on which to apply the generator
-		std::random_device rd;
-		std::default_random_engine generator(rd());
-		std::uniform_int_distribution<long unsigned> distribution(0, 1e9);
-
-		int dispInterval = pow(10, std::max((int)(log10(pointsToDo) - 2), 0));
-
-		int pointsDone = 0;
-		while (pointsDone < pointsToDo) {
-
-			// Find sensible start point
-			double a = distribution(generator) / 1.e9;
-			double b = distribution(generator) / 1.e9;
-			std::complex<double> c0(a * 4 - 2, b * 4 - 2);
-			double q = pow(real(c0) - 0.25, 2) + pow(imag(c0), 2);
-
-			while ((pow(real(c0) + 1, 2) + pow(imag(c0), 2) < 0.0625)		// Testing if in small circle
-				|| (q*(q + real(c0) - 0.25) < 0.25 * pow(imag(c0), 2))	// Testing if in main cardioid
-				|| (abs(c0) > 2)) {										// Testing if outside large circle
-
-				a = distribution(generator) / 1.e9;
-				b = distribution(generator) / 1.e9;
-				c0 = std::complex<double>(a * 4 - 2, b * 4 - 2);
-				q = pow(real(c0) - 0.25, 2) + pow(imag(c0), 2);
-			}
-
-			// Iterate until max is reached, or it diverges
-			std::complex<double> c = c0;
-			int iter = 0;
-
-			while (iter <= max) {
-				c = c * c + c0;
-				if (abs(c) > 2) {
-					break;
-				}
-				iter++;
-			}
-
-			// Add point to matrix if it diverges at correct moment
-			if (iter >= min && iter <= max) {
-				c = c0;
-				for (int i = 0; i <= iter; i++) {
-					int x = (int)(xDet * (real(c) - xMin) / (xMax - xMin));
-					int y = (int)(yDet * (imag(c) - yMin) / (yMax - yMin));
-					if (x >= 0 && x < xDet && y >= 0 && y < yDet) {
-						count[x*yDet + y]++;
-					}
-					c = c * c + c0;
-				}
-				pointsDoneTot++;
-				pointsDone++;
-				if (pointsDone % dispInterval == 0) {
-					std::cout << "Completed: " << pointsDone << " out of " << pointsToDo << " points (" << 100.*pointsDone/pointsToDo << "%, " << pointsDoneTot << " total points)" << std::endl;
-				}
-			}
-		}
-		std::cout << "Finished calculating matrix (" << toc() << "s).\n\n";
 	}
 
 	// Creates a buddhabrot in the matrix, using calculations with doubles (FAST)
@@ -195,7 +139,7 @@ public:
 		int dispInterval = pow(10, std::max((int)(log10(pointsToDo) - 2), 0));
 
 		double a, b, re, re0, im, im0, q, re_, iter, x, y;
-		int pointsDone = 0;
+		double pointsDone = 0;
 		while (pointsDone < pointsToDo) {
 
 			// Find sensible start point
@@ -203,25 +147,25 @@ public:
 			b = distribution(generator) / 1.e9;
 			re = a * 4 - 2;
 			im = b * 4 - 2;
-			q = (re-0.25)*(re-0.25) + im*im;
+			q = (re - 0.25) * (re - 0.25) + im * im;
 			if (countDist) dist[0]++;
 
-			while (((re+1)*(re+1) + im*im < 0.0625)		// Testing if in small circle
-				|| (q*(q + re - 0.25) < 0.25 * im*im)	// Testing if in main cardioid
-				|| (re*re + im*im > 4)) {				// Testing if outside large circle
-				
+			while (((re + 1) * (re + 1) + im * im < 0.0625)		// Testing if in small circle
+				|| (q * (q + re - 0.25) < 0.25 * im * im)	// Testing if in main cardioid
+				|| (re * re + im * im > 4)) {				// Testing if outside large circle
+
 				a = distribution(generator) / 1.e9;
 				b = distribution(generator) / 1.e9;
 				re = a * 4 - 2;
 				im = b * 2 - 2;
-				q = (re-0.25)*(re-0.25) + im*im;
+				q = (re - 0.25) * (re - 0.25) + im * im;
 				if (countDist) dist[0]++;
 			}
 
 			// Iterate until max is reached, or it diverges
 			re0 = re;
 			im0 = im;
-			for (iter = -1; (iter <= max) && (re*re + im*im < 4); iter++) {
+			for (iter = -1; (iter <= max) && (re * re + im * im < 4); iter++) {
 				re_ = re;
 				re *= re;
 				re += re0 - im * im;
@@ -233,14 +177,14 @@ public:
 			if (countDist) dist[iter]++;
 
 			// Add point to matrix if it diverges at correct moment
-			if (iter >= min && iter <= max) {
+			if (iter >= min && iter <= max) {// && int(iter) % 2 == 0) {
 				re = re0;
 				im = im0;
 				for (int i = 0; i <= iter; i++) {
 					x = (int)(xDet * (re - xMin) / (xMax - xMin));
 					y = (int)(yDet * (im - yMin) / (yMax - yMin));
 					if (x >= 0 && x < xDet && y >= 0 && y < yDet) {
-						count[x*yDet + y]++;
+						count[x * yDet + y]++;
 					}
 					re_ = re;
 					re *= re;
@@ -250,8 +194,8 @@ public:
 				}
 				pointsDoneTot++;
 				pointsDone++;
-				if (pointsDone % dispInterval == 0) {
-					std::cout << "Completed: " << pointsDone << " out of " << pointsToDo << " points (" << 100.*pointsDone / pointsToDo << "%, " << pointsDoneTot << " total points)" << std::endl;
+				if (std::fmod(pointsDone, dispInterval) < 1) {
+					std::cout << "Completed: " << pointsDone << " out of " << pointsToDo << " points (" << 100. * pointsDone / pointsToDo << "%, " << pointsDoneTot << " total points)" << std::endl;
 				}
 			}
 		}
@@ -261,8 +205,8 @@ public:
 	// Fills count matrix with a gradient
 	void TestMatrix() {
 
-		for (int i = 0; i < xDet*yDet; i++) {
-			count[i] = int(255 * (float(i%xDet) / (xDet - 1) + float(i/yDet) / (yDet - 1)) * 0.5);
+		for (int i = 0; i < xDet * yDet; i++) {
+			count[i] = int(255 * (float(i % xDet) / (xDet - 1) + float(i / yDet) / (yDet - 1)) * 0.5);
 		}
 	}
 
@@ -285,7 +229,7 @@ public:
 	// Finds greatest value in matrix
 	int FindMaxVal() {
 		int maxVal = -1;
-		for (int i = 0; i < xDet*yDet; i++) {
+		for (int i = 0; i < xDet * yDet; i++) {
 			if (count[i] > maxVal) {
 				maxVal = count[i];
 			}
@@ -303,18 +247,36 @@ public:
 		for (int i = 0; i <= maxVal; i++) {
 			histogram[i] = 0;
 		}
-		for (int i = 0; i < xDet*yDet; i++) {
+		for (int i = 0; i < xDet * yDet; i++) {
 			histogram[count[i]]++;
 		}
 
 		int pointsTot = xDet * yDet;
 		int pointsLeft = xDet * yDet * percentile / 100;
+
+		if (true) { // Print stuff to help me think about how to crop outliers
+			std::ofstream myfile;
+			myfile.open("test.csv");
+
+			float pointsCounted = 0;
+			float percentsCounted = 0;
+			for (int i = 0; i <= maxVal; i++) {
+				pointsCounted += histogram[i];
+				while (percentsCounted / 1000. <= pointsCounted / pointsTot) {
+					myfile << i << "\n";
+					percentsCounted++;
+				}
+			}
+			myfile.close();
+		}
+
 		for (int i = maxVal; i > 0; i--) {
 			pointsTot -= histogram[i];
 			if (pointsTot <= pointsLeft) {
 				return i;
 			}
 		}
+
 		return 1;
 	}
 
@@ -336,7 +298,7 @@ public:
 		int cutoff = FindPercentileVal(percentile);
 		for (int y = yDet - 1; y >= 0; y--) {
 			for (int x = 0; x < xDet; x++) {
-				a = std::min(count[x*yDet + y], cutoff) / (cutoff*1.);
+				a = std::min(count[x * yDet + y], cutoff) / (cutoff * 1.);
 				std::tuple<int, int, int> rgb = find_rgb(a, palette);
 				img << std::get<0>(rgb) << " " << std::get<1>(rgb) << " " << std::get<2>(rgb) << std::endl;
 			}
@@ -345,12 +307,12 @@ public:
 	}
 
 	// Creates a BMP file of the matrix
-	void PrintBMP(float percentile, const std::string& palette, const std::string& filename = "Buddhabrot") {
+	void PrintBMP(const std::string& filename, const std::string& palette, const std::string& trimMethod, double trimVal, float powVal, bool mirror, float gradVal) {
 		std::cout << "Saving BMP image file...\n";
 		tic();
 
 		unsigned int headers[13];
-		FILE * outfile;
+		FILE* outfile;
 		int extrabytes;
 		int paddedsize;
 		int x; int y; int n;
@@ -383,7 +345,7 @@ public:
 		headers[12] = 0;                   // biClrImportant
 
 		std::ostringstream oss;
-		oss << filename << " [" << xDet << "x" << yDet << "] [" << min << "-" << max << "] " << pointsDoneTot << " points.bmp";
+		oss << filename << " [" << xDet << "x" << yDet << "] [" << min << "-" << max << "] " << pointsDoneTot << " points [" << trimMethod << " " << trimVal << " pow " << powVal << "].bmp";
 		std::string fullFilename = oss.str();
 		outfile = fopen(fullFilename.c_str(), "wb");
 
@@ -414,11 +376,37 @@ public:
 
 		// Write data after headers are completed
 		double a;
-		int cutoff = FindPercentileVal(percentile);
+		if (trimMethod == "Trim") trimVal = FindPercentileVal(trimVal);
+
 		for (x = xDet - 1; x >= 0; x--) {
 			for (y = yDet - 1; y >= 0; y--) {					// BMP image format is written from bottom to top
 
-				a = std::min(count[x*yDet + y], cutoff) / (cutoff*1.);
+				a = count[x * yDet + y];
+
+				// Mirror horizontally 
+				if (mirror) {
+					a += count[x * yDet + (yDet - y - 1)];
+				}
+
+				// Apply correct Transformation to correct pixel magnitude distribution
+				if (trimMethod == "Trim") a = std::min(a, trimVal) / (trimVal * 1.);
+				if (trimMethod == "Logistic") a = 2. / (1. + exp(-a / trimVal)) - 1;
+
+				// Apply power to correct pixel magnitude distribution
+				a = pow(a, powVal);
+
+				// Add gradient
+				if (gradVal > 0) {
+					float xT = (float)(xDet) * 0.5;
+					float yT = (float)(yDet) * 0.5;
+					float xR = ((float)(x)-xT) / xT;
+					float yR = ((float)(y)-yT) / yT;
+					float rad = xR * xR + yR * yR;
+					if (rad > 1) { rad = 0; }
+					a += rad * gradVal;
+				}
+
+				// Convert scalar into RGB and save to image
 				std::tuple<int, int, int> rgb = find_rgb(a, palette);
 
 				fprintf(outfile, "%c", std::get<2>(rgb));		// Backwards because written in bgr format
@@ -441,7 +429,7 @@ public:
 		tic();
 
 		unsigned int headers[13];
-		FILE * outfile;
+		FILE* outfile;
 		int extrabytes;
 		int paddedsize;
 		int x; int y; int n;
@@ -505,8 +493,8 @@ public:
 
 
 		std::vector<int> countSmooth;
-		countSmooth.resize(xDet*yDet, 0);
-		for (int i = xDet + 1; x < xDet*(yDet - 1) - 1; i++) {
+		countSmooth.resize(xDet * yDet, 0);
+		for (int i = xDet + 1; x < xDet * (yDet - 1) - 1; i++) {
 			countSmooth[i] = (count[i - xDet - 1] + 2 * count[i - xDet] + count[i - xDet + 1] + 2 * count[i - 1] + 4 * count[i] + 2 * count[i + 1] + count[i + xDet - 1] + 2 * count[i + xDet] + count[i + xDet + 1]) / 16.;
 		}
 
@@ -516,7 +504,7 @@ public:
 		for (x = xDet - 1; x >= 0; x--) {
 			for (y = yDet - 1; y >= 0; y--) {					// BMP image format is written from bottom to top
 
-				a = std::min(countSmooth[x*yDet + y], cutoff) / (cutoff*1.);
+				a = std::min(countSmooth[x * yDet + y], cutoff) / (cutoff * 1.);
 				std::tuple<int, int, int> rgb = find_rgb(a, palette);
 
 				fprintf(outfile, "%c", std::get<2>(rgb));		// Backwards because written in bgr format
@@ -544,7 +532,7 @@ public:
 
 		std::ofstream outFile(fullFilename);
 		outFile << pointsDoneTot << "\n" << min << "\n" << max << "\n" << xDet << "\n" << yDet << "\n" << xMin << "\n" << xMax << "\n" << yMin << "\n" << yMax << "\n";
-		for (int i = 0; i < xDet*yDet; i++) {
+		for (int i = 0; i < xDet * yDet; i++) {
 			outFile << count[i] << "\n";
 		}
 		std::cout << "Finished saving matrix text file " << toc() << "s).\n\n";
@@ -560,7 +548,7 @@ public:
 		std::string fullFilename = oss.str();
 		std::ofstream outFile(fullFilename);
 
-		for (int i = 0; i < max+2; i++) {
+		for (int i = 0; i < max + 2; i++) {
 			outFile << dist[i] << '\n';
 		}
 		std::cout << "Finished saving distribution text file (" << toc() << "s).\n\n";
@@ -569,17 +557,26 @@ public:
 
 int main() {
 
-	// Create new buddhabrot, or load existing text file
-	Buddhabrot BB(2e4, 6e4, 2500, 2500, -1.6, 1.2, -1.4, 1.4);
-	//Buddhabrot BB("Sizetest [2500x2500] [20000-60000] 40 points.txt");
+	float minVal = 12;
+	float maxVal = 36;
+	double numVals = 1e8;
+	int canvasSize = 2500;
 
-	// Calculate points in Buddhabrot
-	BB.CalcDouble(40, false);
+	Buddhabrot BB(minVal, maxVal, canvasSize, canvasSize, -1.999, 1.999, -1.999, 1.999);//-1.6, 1.2, -1.4, 1.4);
 
-	// Save Buddhabrot as text file and as image
-	BB.SaveMatrix("Test");
-	BB.PrintBMP(99.2, "Gold", "Test");
+	// Calculate Buddhabrot
+	BB.CalcDouble(numVals, false);
+	BB.SaveMatrix("Buddhabrot");
 
+	// Or load existing text file
+	Buddhabrot BB("Buddhabrot [2500x2500] [12-36] 1e+8 points.txt");
+
+	// Print buddhabrot
+	bool mirror = true;
+	float logisticVal = numVals * ((maxVal - minVal) / (log(maxVal) - log(minVal))) * (1 + mirror) / (canvasSize * canvasSize) * 3; 
+	float powVal = 1.3;
+	float gradVal = 0.1;
+	BB.PrintBMP("printboibrot", "Gold", "Logistic", logisticVal, 1.2, mirror, gradVal);
 
 	std::cout << "Press any key to continue..." << std::endl;
 	std::getchar();
